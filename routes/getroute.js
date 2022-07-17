7c7c589efeb0cb9ce4b307d598df90c4d9fa237e
gshemdoe <georgehmariki@gmail.com>
2022-07-18 00:54:42 +0300

    support backup

--- routes/getroute.js
+++ routes/getroute.js
@@ -573,4 +573,10 @@ router.get('/dsuser/info/:pid/:uid', async (req, res) => {
 
 })
 
+//to support font5 backup for now (delete it after)
+router.get('/users-ds/table', async (req, res)=> {
+    let ranks = await botUsersModel.find().limit(25).sort('-downloaded')
+    res.send(ranks)
+})
+
 module.exports = router
