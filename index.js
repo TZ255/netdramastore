afeac4d508e4f70137424e75de7bd6e2a175bc19
gshemdoe <georgehmariki@gmail.com>
2023-11-24 18:13:57 +0300

    updated

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 app.use(cors())
-app.set('trust proxy', 1) //our app is hosted on server using proxy to pass user request
+app.set('trust proxy', 2) //our app is hosted on server using proxy to pass user request
 app.use(limiter)
 app.use(postRouter)
 app.use(getRouter)
