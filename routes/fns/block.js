c3fe3551e0a414a9988695fa290b146cad03cd4c
gshemdoe <georgehmariki@gmail.com>
2025-02-03 23:54:43 +0300

    UPDATED

--- routes/fns/block.js
+++ routes/fns/block.js
@@ -8,7 +8,7 @@ const blockReq = async (req, res) => {
         let bet_ad_code = '404'
         const latest_episodes = await episodeModel.find().sort('-createdAt').limit(25)
         const popular = await newDramaModel.find().sort('-thisMonth').limit(25)
-        return res.status(200).render('maintenance.ejs', { popular, latest_episodes, bet_ad })
+        return res.status(200).render('maintenance.ejs', { popular, latest_episodes, bet_ad_code })
     } catch (error) {
         console.error(error)
         res.status(404).send("Oh! Shit.. We have a problem. Please comeback later")
