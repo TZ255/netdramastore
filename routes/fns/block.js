2bdeafc565f48af05d69096e586d455824805e08
gshemdoe <georgehmariki@gmail.com>
2025-02-03 16:33:59 +0300

    updated

--- routes/fns/block.js
+++ routes/fns/block.js
@@ -1,5 +1,15 @@
-const blockReq = (req, res) => {
-    return res.status(404).render('maintenance.ejs')
+const episodeModel = require('../../models/dramastore-episode')
+const newDramaModel = require('../../models/vue-new-drama')
+
+const blockReq = async (req, res) => {
+    try {
+        const latest_episodes = await episodeModel.find().sort('-createdAt').limit(25)
+        const popular = await newDramaModel.find().sort('-thisMonth').limit(25)
+        return res.status(200).render('maintenance.ejs', { popular, latest_episodes })
+    } catch (error) {
+        console.error(error)
+        res.status(404).send("Oh! Shit.. We have a problem. Please comeback later")
+    }
 }
 
 module.exports = blockReq
